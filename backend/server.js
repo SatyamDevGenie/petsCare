56f647ba30b670043f6b7663988726c4950f8ea8
SatyamDevGenie <satyamsawant54@gmail.com>
2025-01-04 04:45:35 +0530

    userRegister - Backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 import dotenv from "dotenv";
 import express from "express";
 import chalk from "chalk";
-
 import connectDB from "./config/db.js";
+import userRoutes from "../backend/routes/userRoutes.js"
 
 
 dotenv.config(); // For Env
@@ -11,6 +11,9 @@ connectDB(); // connection to Mongodb
 
 const app = express();
 app.use(express.json()); // Accepting the json data
+
+// Routes
+app.use("/api/users", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("API is running");
