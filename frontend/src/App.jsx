49f11247de681495d542106d51c6b8ad36bd6f86
SatyamDevGenie <satyamsawant54@gmail.com>
2025-01-03 07:55:16 +0530

    Project Setup + Tailwind Installation Done

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-black text-white font-extrabold text-4xl">
+      Parenting petsCare Application
+    </div>
+  );
+}
+
+export default App;
