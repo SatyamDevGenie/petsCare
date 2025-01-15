f4cbb15d47d37740ebcddd67c97715656ec0becd
SatyamDevGenie <satyamsawant54@gmail.com>
2025-01-16 03:48:54 +0530

    Installed TypeScript - Frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
-function App() {
+import React from "react";
+
+const App: React.FC = () => {
   return (
     <div className="h-screen flex items-center justify-center bg-black text-white font-extrabold text-4xl">
       petsCare Application 2025
     </div>
   );
-}
+};
 
 export default App;
