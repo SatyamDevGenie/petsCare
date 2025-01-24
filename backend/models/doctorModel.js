1af65210717826454075a00f8d1c576ca309fb7f
SatyamDevGenie <satyamsawant54@gmail.com>
2025-01-24 10:00:50 +0530

    doctorModel & appointmentModel created in Backend

---
+++ backend/models/doctorModel.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+const doctorSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    specialization: {
+      type: String,
+      required: true,
+    },
+    contactNumber: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Doctor = mongoose.model("Doctor", doctorSchema);
+
+export default Doctor;
