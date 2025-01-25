d651e6db4827947fa771aeab5b2b860e22f0a5d8
SatyamDevGenie <satyamsawant54@gmail.com>
2025-01-26 03:05:02 +0530

    Integrated services.js in seeder.js - Backend

--- backend/seeder.js
+++ backend/seeder.js
@@ -3,10 +3,12 @@ import connectDB from "./config/db.js";
 import pets from "./data/pets.js";
 import users from "./data/users.js";
 import doctors from "./data/doctors.js";
+import services from "./data/services.js"; // Import services data
 import Pet from "./models/petModel.js";
 import User from "./models/userModel.js";
 import Doctor from "./models/doctorModel.js";
 import Appointment from "./models/appointmentModel.js";
+import Service from "./models/serviceModel.js"; // Import the Service model
 
 dotenv.config();
 
@@ -19,6 +21,7 @@ const importData = async () => {
     await Pet.deleteMany();
     await User.deleteMany();
     await Appointment.deleteMany();
+    await Service.deleteMany(); // Delete existing services data
 
     // Insert user data and get their IDs
     const createdUsers = await User.insertMany(users);
@@ -41,6 +44,15 @@ const importData = async () => {
     // Insert doctors data into the database
     await Doctor.insertMany(doctors);
 
+    // Insert services data into the database
+    const sampleServices = services.map((service) => ({
+      title: service.title,
+      description: service.description,
+      price: service.price,
+      image: service.image, // Optional, omit if not needed
+    }));
+    await Service.insertMany(sampleServices); // Insert services into the database
+
     console.log("Data imported successfully!");
     process.exit();
   } catch (err) {
@@ -51,11 +63,12 @@ const importData = async () => {
 
 const destroyData = async () => {
   try {
-    // Delete all pets, users, doctors data
+    // Delete all pets, users, doctors, and services data
     await Doctor.deleteMany();
     await Pet.deleteMany();
     await User.deleteMany();
     await Appointment.deleteMany();
+    await Service.deleteMany(); // Delete services data
 
     console.log("Data destroyed!");
     process.exit();
